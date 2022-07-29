98356996d681a6a5e8f1e5c662d2085bd19235f1
Rob Howell <rbh3457@gmail.com>
2022-07-29 10:28:02 -0400

    Add files via upload

--- MyClient.java
+++ MyClient.java
@@ -1,106 +1,129 @@
-import java.io.*;  
-import java.net.*;  
+import java.io.*;
+import java.net.*;
+import java.rmi.Remote;
 import java.util.Scanner;
+import java.util.function.DoubleToIntFunction;
 import java.util.ArrayList;
-public class MyClient {  
-public static void main(String[] args)throws Exception{ 
-MyClient myChoice = new MyClient();
-myChoice.choiceLoop(); //runs the choice loop
-}
-  
 
-public void choiceLoop()throws Exception{ 
-{
-    Scanner kybd = new Scanner(System.in);
-    Socket s=new Socket("localhost",6666);  //creates the socket and its address
+public class MyClient {
+    public static void main(String[] args) throws Exception {
+        Scanner kybd = new Scanner(System.in);
+        Socket s = new Socket("localhost", 6666); // creates the socket and its address
+        ArrayList<String> data = new ArrayList<String>(); // initializes data so it exists before the loop
+        MyClient myChoice = new MyClient();
 
-    DataOutputStream dout=new DataOutputStream(s.getOutputStream()); //creates the output stream
-    ArrayList<String> data = new ArrayList<String>(); //initializes data so it exists before the loop
-    String x = "";
-    String oper = "";
-    Double firstNum = 0.0;
-    Double secNum = 0.0;
-    String eq = "";
-    while(!x.equalsIgnoreCase("quit"))
-    {
-        System.out.println("Type PRINT to print server, type QUIT to exit the program, or type BEGIN to begin");
-        x = kybd.next();
+        String pick = "";
+        while (!pick.equalsIgnoreCase("Begin")) { //begins the loop to allow user inout
+            System.out.println("Type BEGIN to begin");
+            pick = kybd.next();
+        }
+        myChoice.choiceLoop(kybd, s, data, myChoice); // runs the choice loop
+    }
 
-        if(x.equalsIgnoreCase("print"))
+    public void print(Scanner kybd, Socket s, ArrayList<String> data, DataOutputStream dout, MyClient myChoice) throws Exception { //method to print inputs
         {
-            for(int i =0; i<data.size(); i++) //sends each entry of the arraylist across the socket
+            DataInputStream din=new DataInputStream(s.getInputStream()); //starts a new data input stream to recieve data from the server
+            boolean mode = true; //boolean switch 
+            String str;
+            while(mode == true) 
             {
-                dout.writeUTF(i + ". )" + data.get(i)); //writes to socket
-                dout.flush(); //clears socket
-            }   
-            dout.close(); //closes stream 
-            s.close(); //closes socket
-            return;
-        }else if(x.equalsIgnoreCase("quit"))
+                str = (String) din.readUTF();  //recieve strings from the server
+                data.add(str);//add these strings to an arraylist
+                if(str.equals("")) //if the recieved string is empty set the switch to false
+                {
+                    mode = false; //ends the while loop 
+                }
+            }
+            data.remove(data.get(data.size()-1)); //removes the last empty string 
+            for(int i = 0; i<data.size(); i++)
+            {   
+                System.out.println(data.get(i)); //prints the arraylist
+
+            }
+        }
+    }
+
+    public void quit(Socket s, DataOutputStream dout) throws Exception {
         {
-            break;
-        }else if(x.equalsIgnoreCase("begin"))
+            dout.close(); // closes stream
+            s.close(); // closes socket
+        }
+    }
+
+    public void choiceLoop(Scanner kybd, Socket s, ArrayList<String> data, MyClient myChoice) throws Exception {
         {
-            Double outpt = 0.0;
-            System.out.println("Input your first number: ");
-            firstNum = kybd.nextDouble();
-            System.out.println("Input the operator (+,-,*,/,^): ");
-            oper = kybd.next();
-            System.out.println("Input your second number: ");
-            secNum = kybd.nextDouble();
-            eq = firstNum.toString() + " " + oper + " " + secNum.toString() + " = ";
-            System.out.println("The equation is: "+ eq);
-            switch(oper)
-            {
-                case "+" :
-                {
-                    outpt = firstNum+secNum;
-                    System.out.println("The answer is: "+outpt);
-                    String ans = (eq + outpt); 
-                    data.add(ans.toString());
-                    break;
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream()); // creates the output stream
+            dout.flush();//flushes the socket
+            String x = "";
+            while (!x.equalsIgnoreCase("quit")) {
+                try{
+                x = "";
+                String oper = "";
+                String num2 = "";
+                String eq = "";
+                System.out.println("Input your first number or the command to PRINT or QUIT: ");
+                x = kybd.next();
+                if (x.equalsIgnoreCase("print")) {
+                    dout.writeUTF(x);//sends print to the server
+                    myChoice.print(kybd, s, data, dout, myChoice); // runs the print method
+                } else if (x.equalsIgnoreCase("quit")) {
+                    myChoice.quit(s, dout);//runs quit method
+                } else if (!x.equalsIgnoreCase("print") || !x.equalsIgnoreCase("quit")) {
+                    System.out.println("Input the operator (+,-,*,/,^): ");   
+                    dout.writeUTF(x); //writes the first number to the server if it is not print
+                    oper = kybd.next();
+                    System.out.println("Input your second number: ");
+                    num2 = kybd.next();
+                    dout.writeUTF(num2);//writes second number to server
+                    switch(oper) //sends operator and full equation to server as strings depending on the operator chosen
+                    {
+                        case "+": {
+                            dout.writeUTF(oper);
+                            eq = x + " " + oper + " " + num2 + " = ";
+                            dout.writeUTF(eq);
+                            System.out.println("The equation is: " + eq);        
+                            continue;
+                        }
+                        case "-": {
+                            dout.writeUTF(oper);
+                            eq = x + " " + oper + " " + num2 + " = ";
+                            dout.writeUTF(eq);
+                            System.out.println("The equation is: " + eq);        
+                            continue;
+                        }
+                        case "*": {
+                            dout.writeUTF(oper);
+                            eq = x + " " + oper + " " + num2 + " = ";
+                            dout.writeUTF(eq);
+                            System.out.println("The equation is: " + eq);        
+                            continue;
+                        }
+                        case "/": {
+                            dout.writeUTF(oper);
+                            eq = x + " " + oper + " " + num2 + " = ";
+                            dout.writeUTF(eq);
+                            System.out.println("The equation is: " + eq);        
+                            continue;
+                        }
+                        case "^": {
+                            dout.writeUTF(oper);
+                            eq = x + " " + oper + " " + num2 + " = ";
+                            dout.writeUTF(eq);
+                            System.out.println("The equation is: " + eq);        
+                            continue;
+                        }
+                        default: {
+                            System.out.println("Invalid expression invalid operator");
+                            dout.writeUTF(oper);
+                            eq = x + " " + oper + " " + num2 + " = ";
+                            dout.writeUTF(eq);
+                            choiceLoop(kybd, s, data, myChoice);
+                            return;
+                        }
+                    }
                 }
-                case "-" :
-                {
-                    outpt = firstNum-secNum;
-                    System.out.println("The answer is: "+outpt); 
-                    String ans = (eq + outpt); 
-                    data.add(ans.toString());
-                    break;
-                }
-                case "*" :
-                {
-                    outpt = firstNum*secNum;
-                    System.out.println("The answer is: "+outpt);
-                    String ans = (eq + outpt); 
-                    data.add(ans.toString());
-                    break;
-                }
-                case "/" :
-                {
-                    outpt = (Double)firstNum/secNum;
-                    System.out.println("The answer is: "+outpt); 
-                    String ans = (eq + outpt); 
-                    data.add(ans.toString());
-                    break;
-                }
-                case "^" :
-                {
-                    outpt = (Double)Math.pow(firstNum,secNum);
-                    System.out.println("The answer is: "+outpt); 
-                    String ans = (eq + outpt); 
-                    data.add(ans.toString());
-                    break;
-                }
-                default :
-                {
-                    System.out.println("Invalid expression invalid operator");
-                    choiceLoop();
-                    return;
-                }
-            }   
-        }   
+            }catch(IOException e){}
+        }
     }
 }
-}  
 }
--- MyServer.java
+++ MyServer.java
@@ -1,26 +1,224 @@
-import java.io.*;  
-import java.net.*;  
-public class MyServer {  
-public static void main(String[] args)throws Exception{   
-ServerSocket ss=new ServerSocket(6666);  
-Socket s=ss.accept();//establishes connection   
-DataInputStream dis=new DataInputStream(s.getInputStream());  
-//String  str=(String)dis.readUTF();
-while(true)
-{  
-    try {
-        String  str= "null";
-        str = (String)dis.readUTF();
-        if(str == "")
-        {
-            break;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class MyServer {
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(6666);
+        ArrayList<String> data = new ArrayList<String>(); // initializes data so it exists before the loop
+        Socket s = ss.accept();// establishes connection
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream()); // creates the output stream
+        MyServer myMethod = new MyServer();
+        myMethod.loop(data, dis, dout);
+    }
+
+    public void loop(ArrayList<String> data, DataInputStream dis, DataOutputStream dout)
+    {
+        while (true) {
+            try {
+                String eq = "";
+                String oper = "";
+                String num2 = "";
+                String x = "";
+                Double firstNum = 0.0;
+                Double secNum = 0.0;
+                Double outpt = 0.0;
+                String ans = "";
+                boolean isNum1 = false;
+                boolean isNum2 = false;
+                String outptCase2 = "";
+                String outptCase3 = "";
+                String outptCase4 = "";
+                x = (String) dis.readUTF();
+                if(x.equalsIgnoreCase("print"))
+                {
+                    try{
+                    for(int i = 0; i < data.size(); i++)
+                    {
+                        dout.writeUTF(data.get(i));
+                    }
+                    data.add("");
+                    dout.writeUTF(data.get(data.size()-1));
+                    for(int u = data.size(); u > 0 ; u--)
+                    {
+                        data.remove(0); //removes all entries from arraylist
+                    }
+                    loop(data, dis, dout);
+                }catch(IOException e){}
+                }
+                num2 = (String) dis.readUTF();
+                oper = (String) dis.readUTF();
+                eq = (String) dis.readUTF();
+                isNum1 = x.matches(".*[0-9].*");
+                isNum2 = num2.matches(".*[0-9].*");
+                if (isNum1 == true) {
+                    firstNum = Double.parseDouble(x);
+                }
+                if (isNum2 == true) {
+                    secNum = Double.parseDouble(num2);
+                }
+                if (isNum1 == true && isNum2 == true) {
+                    switch (oper) {
+                        case "+": {
+                            outpt = firstNum + secNum;
+                            System.out.println("The answer is: " + outpt);
+                            ans = (eq + outpt);
+                            data.add(ans);
+                            // dout.flush(); // clears socket
+                            break;
+                        }
+                        case "-": {
+
+                            outpt = firstNum - secNum;
+                            System.out.println("The answer is: " + outpt);
+                            ans = (eq + outpt);
+                            data.add(ans);
+                            break;
+                        }
+                        case "*": {
+                            outpt = firstNum * secNum;
+                            System.out.println("The answer is: " + outpt);
+                            ans = (eq + outpt);
+                            data.add(ans);
+                            break;
+                        }
+                        case "/": {
+                            if (secNum == 0) {
+                                System.out.println("Invalid expression cannot divide by zero");
+                                // choiceLoop(kybd, s, data, myChoice);
+                            }
+                            outpt = (Double) firstNum / secNum;
+                            System.out.println("The answer is: " + outpt);
+                            ans = (eq + outpt);
+                            data.add(ans);
+                            break;
+                        }
+                        case "^": {
+                            if (firstNum < 0 && secNum == 0.5) {
+                                System.out
+                                        .println(
+                                                "Invalid expression cannot take the square root of a negative number");
+                                // choiceLoop(kybd, s, data, myChoice);
+                            }
+                            outpt = (Double) Math.pow(firstNum, secNum);
+                            System.out.println("The answer is: " + outpt);
+                            ans = (eq + outpt);
+                            data.add(ans);
+                            break;
+                        }
+                        default: {
+                            System.out.println("Invalid expression invalid operator");
+                            loop(data, dis, dout);
+                            // choiceLoop(kybd, s, data, myChoice);
+                            return;
+                        }
+                    }
+                } else if (isNum1 == true && isNum2 == false) {
+                    switch (oper) {
+                        case "+": {
+                            outptCase2 = firstNum.toString() + " + " + num2 + " = ?";
+                            data.add(outptCase2);
+                            break;
+                        }
+                        case "-": {
+                            outptCase2 = firstNum.toString() + " - " + num2 + " = ?";
+                            data.add(outptCase2);
+                            break;
+                        }
+                        case "*": {
+                            outptCase2 = firstNum.toString() + " * " + num2 + " = ?";
+                            data.add(outptCase2);
+                            break;
+                        }
+                        case "/": {
+                            outptCase2 = firstNum.toString() + " / " + num2 + " = ?";
+                            data.add(outptCase2);
+                            break;
+                        }
+                        case "^": {
+                            outptCase2 = firstNum.toString() + " ^ " + num2 + " = ?";
+                            data.add(outptCase2);
+                            break;
+                        }
+                        default: {
+                            System.out.println("Invalid expression invalid operator");
+                            loop(data, dis, dout);
+                            // choiceLoop(kybd, s, data, myChoice);
+                            return;
+                        }
+                    }
+                } else if (isNum1 == false && isNum2 == true) {
+                    switch (oper) {
+                        case "+": {
+                            outptCase3 = x + " + " + secNum.toString() + " = ?";
+                            data.add(outptCase3);
+                            break;
+                        }
+                        case "-": {
+                            outptCase3 = x + " - " + secNum.toString() + " = ?";
+                            data.add(outptCase3);
+                            break;
+                        }
+                        case "*": {
+                            outptCase3 = x + " * " + secNum.toString() + " = ?";
+                            data.add(outptCase3);
+                            break;
+                        }
+                        case "/": {
+                            outptCase3 = x + " / " + secNum.toString() + " = ?";
+                            data.add(outptCase3);
+                            break;
+                        }
+                        case "^": {
+                            outptCase3 = x + " ^ " + secNum.toString() + " = ?";
+                            data.add(outptCase3);
+                            break;
+                        }
+                        default: {
+                            System.out.println("Invalid expression invalid operator");
+                            loop(data, dis, dout);
+                            // choiceLoop(kybd, s, data, myChoice);
+                            return;
+                        }
+                    }
+                } else if (isNum1 == false && isNum2 == false) {
+                    switch (oper) {
+                        case "+": {
+                            outptCase4 = x + " + " + num2 + " = ?";
+                            data.add(outptCase4);
+                            break;
+                        }
+                        case "-": {
+                            outptCase4 = x + " - " + num2 + " = ?";
+                            data.add(outptCase4);
+                            break;
+                        }
+                        case "*": {
+                            outptCase4 = x + " * " + num2 + " = ?";
+                            data.add(outptCase4);
+                            break;
+                        }
+                        case "/": {
+                            outptCase4 = x + " / " + num2 + " = ?";
+                            data.add(outptCase4);
+                            break;
+                        }
+                        case "^": {
+                            outptCase4 = x + " ^ " + num2 + " = ?";
+                            data.add(outptCase4);
+                            break;
+                        }
+                        default: {
+                            System.out.println("Invalid expression invalid operator");
+                            loop(data, dis, dout);
+                            // choiceLoop(kybd, s, data, myChoice);
+                            return;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+            }
         }
-        System.out.println(str);  
-        ss.close();
-    }catch  (EOFException e){
-        System.out.println("End of list");
-        break;
-    }catch (IOException e) {}
-}   
+    }
 }
-}  
